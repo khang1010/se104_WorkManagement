c35c6368c18d8c54794cea18d129db4ba7345be9
Quang Duong <quangduongptsc@gmail.com>
2023-05-10 23:35:19 +0700

    Modify ui user infor

--- app/src/main/java/com/example/workmanagement/activities/UserInforActivity.java
+++ app/src/main/java/com/example/workmanagement/activities/UserInforActivity.java
@@ -1,10 +1,12 @@
 package com.example.workmanagement.activities;
 
 import android.os.Bundle;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,10 +16,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
 import com.example.workmanagement.R;
+import com.example.workmanagement.utils.dto.UserDTO;
+import com.example.workmanagement.utils.services.impl.AuthServiceImpl;
+import com.example.workmanagement.viewmodels.BoardViewModel;
 import com.example.workmanagement.viewmodels.UserViewModel;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserInforActivity extends AppCompatActivity {
     private Button btnBack;
@@ -28,14 +41,15 @@ public class UserInforActivity extends AppCompatActivity {
     private ImageView imgEditBio;
     private ImageView imgEditPhone;
     private ImageView imgEditWork;
-    private TextView txtNothing;
+    private TextView txtNothing, txtName, txtEmail;
+
 
     private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        userViewModel = new ViewModelProvider(UserInforActivity.this).get(UserViewModel.class);
 
         setContentView(R.layout.activity_user_infor);
         btnBack = findViewById(R.id.btn_back);
@@ -47,10 +61,42 @@ public class UserInforActivity extends AppCompatActivity {
         imgEditPhone=findViewById(R.id.img_edit_phone);
         imgEditWork=findViewById(R.id.img_edit_work);
         txtNothing = findViewById(R.id.txt_nothing);
+        txtName = findViewById(R.id.txt_username_user_profiler);
+        txtEmail = findViewById(R.id.text_view_email);
+
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account == null) {
+
+        } else {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setEmail(account.getEmail());
+            userDTO.setDisplayName(account.getDisplayName());
+            userDTO.setFamilyName(account.getFamilyName());
+            userDTO.setGivenName(account.getGivenName());
+            userDTO.setPhotoUrl(String.valueOf(account.getPhotoUrl()));
+
+            txtName.setText(account.getDisplayName());
+            txtEmail.setText(account.getEmail());
+
+            if (!String.valueOf(account.getPhotoUrl()).equals("null"))
+                Glide.with(UserInforActivity.this)
+                        .asBitmap()
+                        .load(String.valueOf(account.getPhotoUrl()))
+                        .into(avatar);
+            else
+                avatar.setImageResource(R.mipmap.ic_launcher);
+        }
 
         txtNothing.setOnClickListener(v ->
                 Toast.makeText(UserInforActivity.this, "Please enter board !!!", Toast.LENGTH_SHORT).show()
         );
+
+
+
+
+
         imgEditBio.setOnClickListener(v -> {
             if(bio.isEnabled())
             {
